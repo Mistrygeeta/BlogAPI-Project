a1a914e915fce773a422a86ffd20f889ad71ed97
Mistrygeeta <geeta.mistry2001@gmail.com>
2026-04-01 12:25:57 +0530

    initialize backend with Express and MongoDB setup
    - Created package.json with necessary dependencies (dotenv, express, mongoose)
    - Implemented server.js to set up the Express server and connect to the database
    - Added app.js to define the Express application
    - Developed blog.controller.js for handling blog creation logic
    - Established db.js for MongoDB connection management
    - Defined blog.model.js for the blog schema
    - Created blog.router.js for routing blog-related requests (incomplete)

---
+++ backend/src/controllers/blog.controller.js
@@ -0,0 +1,37 @@
+const blogModel = require("../models/blog.model");
+
+
+async function createBlogController(req, res) {
+    try {
+    let {title, author, content} = req.body;
+    if(!title || !author || !content){
+        return res.status(422).json({
+            message: "All field are required!"
+        })
+    };
+    let newBlog = await blogModel.create({
+        title,
+        author,
+        content
+    });
+
+    if(!newBlog){
+        return res.status(400).json({
+            message : "error in creating blog!",
+
+        })
+    };
+
+    return res.status(201).json({
+        message: "blog created succesfully",
+        Blog : newBlog
+    })
+    } catch (error) {
+        return res.status(500).json({
+            message : "internal server error",
+            error: message.error
+        })
+    }
+};
+
+module.exports = createBlogController ;
