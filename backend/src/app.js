6df00cca4b0853cea4eeda4b85a97ec34d011aa8
Mistrygeeta <geeta.mistry2001@gmail.com>
2026-04-13 13:09:57 +0530

    add CORS support to the backend and update dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,9 @@
+const cors = require("cors");
 const express = require("express");
 const blogRouter = require("./routers/blog.router")
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/blog",blogRouter)
 
