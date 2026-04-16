10d9015b3b5eba601f96daa684f8ab9b5f2306d0
Mistrygeeta <geeta.mistry2001@gmail.com>
2026-04-16 18:07:32 +0530

    update CORS configuration to allow all origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const blogRouter = require("./routers/blog.router")
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET","POST","PUT","PATCH","DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use("/api/blog",blogRouter)
 
