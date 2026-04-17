ceae3b816a800a58398f475e84ac9b0e3779adb8
Mistrygeeta <geeta.mistry2001@gmail.com>
2026-04-17 11:37:05 +0530

    update CORS configuration to allow multiple origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,10 @@ const blogRouter = require("./routers/blog.router")
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+     "https://blog-api-project-seven.vercel.app/",
+     "http://blog-api-project-seven.vercel.app/"
+    ],
     methods: ["GET","POST","PUT","PATCH","DELETE"],
     credentials: true
 }));
